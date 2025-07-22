7adf2185d36fc21a00466ec5559b82a74484838f
Daniel Long <24.daniel.long@gmail.com>
2025-07-22 16:59:13 -0500

    test(coverage): add coverage thresholds (#81)

--- {{cookiecutter.project_slug}}/tests/math/multiply.test.ts
+++ {{cookiecutter.project_slug}}/tests/math/multiply.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { multiply } from "../src/math/multiply";
+import { multiply } from "../../src/main";
 
 test("multiply 1 and 2 should equal 2", () => {
   expect(multiply(1, 2)).toBe(2);
